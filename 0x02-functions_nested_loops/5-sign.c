a006a2fc0a2778a2202f650fce04f1a730ae3684
BaRzz007 <Bazzdude007@gmail.com>
2022-03-17 12:21:50 -0700

    Created 5-sign.c and updated main.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _print_sign - prints the sign of a number
+ *
+ * @n: parameter of function _print_sign to be tested
+ *
+ * Return: 1 if number is +ve, 0 if number is zero and -1 if nnumber is -ve
+ *
+ **/
+int _print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	return (0);
+}
