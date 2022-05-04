e1fb8818ccd21ebec25df9a6ea804e81b146d952
BaRzz007 <Bazzdude007@gmail.com>
2022-05-04 09:03:02 -0700

    Added 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+include "main.h"
+
+/*
+ * times_table - prints multiplication table up to nine
+ */
+void times_table (void)
+{
+	int i, j, result;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			result = i * j;
+			_putchar(result + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
