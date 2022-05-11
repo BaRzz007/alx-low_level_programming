d5c6e9d7c7fc7ebbba66e418e549651512ab1592
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-11 23:36:25 +0000

    updated

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ void puts_half(char *str)
 	if ((len - 1) % 2 == 0)
 	{
 		i = (len - 1) / 2;
-		while (i < len)
+		while (i <= len)
 		{
 			_putchar(str[i]);
 			i++;
@@ -26,7 +26,7 @@ void puts_half(char *str)
 	else
 	{
 		i = ((len - 1) / 2) + 2;
-		while (i < len)
+		while (i <= len)
 		{
 			_putchar(str[i]);
 			i++;
