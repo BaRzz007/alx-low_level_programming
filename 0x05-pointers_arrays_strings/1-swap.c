30d7af33153bb934f13b90e2d67bf47fd690e971
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-11 00:15:42 +0000

    Added 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values to two integers
+ * @a: first pointer
+ * @b: second pointer
+ */
+void swap_int(int *a, int *b)
+{
+	int *temp;
+
+	a = temp;
+	a = b;
+	b = temp;
+}
