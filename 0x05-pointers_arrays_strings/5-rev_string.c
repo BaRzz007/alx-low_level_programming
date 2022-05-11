649d267bcd82bca1b92d2aeb84a66da7fe5725af
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-11 12:22:48 +0000

    updated

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,14 +6,17 @@
  */
 void rev_string(char *s)
 {
-	int i;
+	int i, j;
+	char *temp;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
+		*(temp + i) = s[i];
 	}
+	j = 0;
 	while ((i - 1) >= 0)
 	{
-		_putchar(*(s + i));
+		s[j] = *(temp + i);
 		i--;
 	}
 }
