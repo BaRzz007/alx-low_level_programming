be8cd163f929e32e5f9e87d21befcbc165c95621
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-11 11:46:26 +0000

    Added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string, in reverse,
+ * followed by a new line
+ * @s: String to be reversed
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	j = i;
+	while (j <= 0)
+	{
+		_putchar(s[j]);
+	}
+}
