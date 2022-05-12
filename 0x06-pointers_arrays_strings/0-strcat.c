966f071f993f933e59569d9656cb1fd36ce8ee3f
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-12 01:43:38 +0000

    updated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,16 +12,17 @@ char *_strcat(char *dest, char *src)
 	int len, i;
 
 	len = 0;
-	while (src[len] != '\0')
+	while (dest[len] != '\0')
 	{
 		len++;
 	}
 	i = 0;
-	while (dest[i] != '\0')
+	while (src[i] != '\0')
 	{
-		src[len] = dest[i];
+		dest[len] = src[i];
 		i++;
 		len++;
 	}
+	dest[len + i] = '\0';
 	return (dest);
 }
