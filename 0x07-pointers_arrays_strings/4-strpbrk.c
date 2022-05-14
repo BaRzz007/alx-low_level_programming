a25c690b67d9149097cb2e9dca6d4940d6b4e7f0
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-14 10:33:41 +0000

    Added 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,3 +7,23 @@
  * Return: pointer to the byte in s that matches one of the bytes in accept
  * or NULL if no such byte is found
  */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, match;
+
+	match = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				match = 1;
+				break;
+			}
+		}
+		if (match == 1)
+			break;
+	}
+	return (s + i);
+}
