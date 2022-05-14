355b2db69ac0e06c7cfa80c5e3432b94e04f2b00
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-14 02:04:04 +0000

    Added 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string to be encoded
+ * Return: pointer to resulting string
+ */
+char *leet(char *str)
+{
+	char c[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't',
+		'T', 'l', 'L'}
+	char num[10] = {'4', '4', '3', '3', '0', '0', '7',
+		'7', '1', '1'}
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; i < sizeof(c) - 1; j++)
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = num[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
