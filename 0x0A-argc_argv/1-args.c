7b710cf54449959e30450829579d662859c20479
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-21 17:00:27 +0000

    created 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to the program
+ * @argc: argument count
+ * @argv: array of string atguments
+ * Return: 0 if success
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
