a90b6b44079a83cc8eb14ca22f5c7e53c70b30f1
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-24 16:12:12 +0000

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,9 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	p = malloc(size * sizeof(char));
+	p = malloc(size * sizeof(c));
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		p[i] = c;
 	return (p);
