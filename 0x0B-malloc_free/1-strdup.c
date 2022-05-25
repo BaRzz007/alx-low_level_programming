533c66849fe20ab98b887b8a4b756093fdcc65e4
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-25 00:15:02 +0000

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -22,7 +22,8 @@ char *_strdup(char *str)
 
 	for (size = 0; str[size] != '\0'; size++)
 		;
-	p = malloc(sizeof(str));
+	size++;
+	p = malloc(size * sizeof(char));
 	if (p == NULL)
 		return (NULL);
 
