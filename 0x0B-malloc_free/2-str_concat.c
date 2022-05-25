03a2585521d662b90b376b79931fe02e671cd85c
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-25 02:06:19 +0000

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,7 +34,7 @@ char *str_concat(char *s1, char *s2)
 			size1++;
 		}
 	}
-	else if (size1 == NULL)
+	else if (s1 == NULL)
 		p[size1] = '\0';
 	size2 = 0;
 	if (s2)
@@ -46,7 +46,7 @@ char *str_concat(char *s1, char *s2)
 			size2++;
 		}
 	}
-	else if (size2 == NULL)
+	else if (s2 == NULL)
 		p[size1] = '\0';
 	p[size1] = '\0';
 	return (p);
