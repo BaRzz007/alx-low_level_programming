fc1b5366821bda3497a413166b613327f52f5862
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-25 01:13:22 +0000

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,18 +25,24 @@ char *str_concat(char *s1, char *s2)
 	if (p == NULL)
 		return (NULL);
 
-	size1 = 0;
-	while (s1 != NULL && size1 < i)
+	if (s1 != NULL)
 	{
-		p[size1] = s1[size1];
-		size1++;
+		size1 = 0;
+		while (s1 != NULL && size1 < i)
+		{
+			p[size1] = s1[size1];
+			size1++;
+		}
 	}
-	size2 = 0;
-	while (s2 != NULL && size2 < (i + j))
+	if (s2 != NULL)
 	{
-		p[size1] = s2[size2];
-		size1++;
-		size2++;
+		size2 = 0;
+		while (size2 < (i + j))
+		{
+			p[size1] = s2[size2];
+			size1++;
+			size2++;
+		}
 	}
 	p[size1] = '\0';
 	return (p);
