92d6dd168ae9a80253d2581d3fda88eea4374c2b
Bassey Ezekiel <bazzdude007@gmail.com>
2022-05-25 14:18:51 +0000

    Updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,9 +21,9 @@ int **alloc_grid(int width, int height)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 			p[i][j] = 0;
 	}
 	return (p);
