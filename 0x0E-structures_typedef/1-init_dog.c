1e33cff59e6c7260c65cd1d8d0cafc89f3974a40
Bassey Ezekiel <bazzdude007@gmail.com>
2022-06-04 00:28:05 +0000

    Create 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a struct dog variable
+ * @name: name of dog
+ * @d: pointer of type struct dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *p;
+
+	p = malloc(sizeof(struct dog));
+	if (p == NULL)
+		return (NULL);
+
+	p = &d;
+	(*p).name = name;
+	(*p).age = age;
+	(*p).owner = owner;
+	/*
+	 * or do this without dereferencing + accessing with the '.' symbol
+	 * p->name = name;
+	 * p->age = age;
+	 * p->owner = owner;
+	 */
+	return (p);
+}
