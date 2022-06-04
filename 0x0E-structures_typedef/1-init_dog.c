0e7078f18d05bccaacb2d902d2d19a5a67538ca0
Bassey Ezekiel <bazzdude007@gmail.com>
2022-06-04 01:07:16 +0000

    Update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,14 +11,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *p;
-
-	p = d;
-	p = malloc(sizeof(struct dog));
-
-	(*p).name = name;
-	(*p).age = age;
-	(*p).owner = owner;
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 
 	/*
 	 * or do this without dereferencing + accessing with the '.' symbol
