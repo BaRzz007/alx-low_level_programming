08b3df12956244f1d085caff6a46bbe68a681810
Bassey Ezekiel <bazzdude007@gmail.com>
2022-06-04 00:09:19 +0000

    Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct to declare properties of dog
+ * @name: name of dog
+ * @owner: name of owner
+ * @age: age of dog
+ */
 struct dog
 {
 	char *name, *owner;
 	float age;
 };
+#endif
