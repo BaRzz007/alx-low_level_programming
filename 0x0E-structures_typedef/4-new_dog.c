b756610b78f141d8d869e49c6294937948c52789
Bassey Ezekiel <bazzdude007@gmail.com>
2022-06-07 08:30:16 +0000

    Update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,11 +15,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
 	char *p_name, *p_owner;
+	unsigned int size1, size2, i;
 
-	p_name = malloc(sizeof(char) * strlen(name));
-	p_owner = malloc(sizeof(char) * strlen(owner));
+	for (size1 = 0; name[size1] != '\0'; size1++)
+		;
+	for (size2 = 0; owner[size2] != '\0'; size2++)
+		;
 
+	p_name = malloc(sizeof(char) * size1 + 1);
+	p_owner = malloc(sizeof(char) * size2 + 1);
 	ptr = malloc(sizeof(dog_t));
+
+	for (i = 0; i < size1; i++)
+		p_name[i] = name[i];
+	for (i = 0; i < size2; i++)
+		p_owner[i] = owner[i];
+
 	if (ptr && p_name && p_owner)
 	{
 		ptr->name = p_name;
@@ -30,5 +41,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p_owner);
 		return (ptr);
 	}
+	free(p_name);
+	free(p_owner);
 	return (NULL);
 }
