cc97dd82d8fd6a4e0a0d4495d8f39c7cd7232924
Bassey Ezekiel <bazzdude007@gmail.com>
2022-06-09 02:10:44 +0000

    Update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
+#include <stddef.h>
 #ifndef FUNCTIONS
 #define FUNCTIONS
-#define unsigned int size_t
 int _putchar(char);
 void print_name(char *, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
