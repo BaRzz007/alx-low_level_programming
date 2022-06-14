3c26cd622848788a64bd6ef6ecd19e744e485532
Bassey Ezekiel <bazzdude007@gmail.com>
2022-06-14 14:03:18 +0000

    Update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,10 +15,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!(h->str))
-		{
 			printf("[0] (nil)\n");
-		}
-		printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		n++;
 	}
