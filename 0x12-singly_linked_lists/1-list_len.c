ae67f6fa72ef37bba6da39dd93a37af9711bb39f
Bassey Ezekiel <bazzdude007@gmail.com>
2022-06-16 00:10:13 +0000

    update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include "lists.h"
+
 
 /**
  * list_len - Returns the number of elements in a linked list_t list
@@ -12,8 +14,8 @@ size_t list_len(const list_t *h)
 	n = 0;
 	while (h)
 	{
-		h = h->next;
 		n++;
+		h = h->next;
 	}
 	return (n);
 }
