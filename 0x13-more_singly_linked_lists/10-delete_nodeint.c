2e2496241f8521da29f485ccecec370fea451ae5
Bassey Ezekiel <bazzdude007@gmail.com>
2022-07-05 11:30:13 +0000

    Betty check

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,8 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *head_ref, *temp;
 	unsigned int index_count;
 
-	
-	if(!(head))
+	if (!(head))
 		return (-1);
 
 	head_ref = *head;
