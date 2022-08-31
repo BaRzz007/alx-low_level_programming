d6bde07ad4f54ee3b167e1927d2918599d56ad55
Ezekiel Bassey <Bazzdude007@gmail.com>
2022-08-31 04:57:23 +0100

    Update 2-add_dnodeint.c, Initial commit 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,6 +4,7 @@
  * add_dnodeint - add new node at the beginning of a dlistint_t list
  * @head: pointer to the head pointer of a dlistint_t list
  * @n: node date
+ * Return: address of new node, NULL if operation fails
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
