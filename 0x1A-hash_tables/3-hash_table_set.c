195332dbb13a01cd17ef2aaf7e4b3fec87aa079e
Ezekiel Bassey <Bazzdude007@gmail.com>
2022-11-19 02:22:29 +0100

    Handled collisions and updates properly

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "hash_tables.h"
 
 /**
@@ -12,41 +13,40 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node;
+	hash_node_t *node, *current;
 
 	if (!key || strlen(key) < 1)
 		return (0);
 
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current)
+	{
+		if (!strcmp(current->key, key))
+		{
+			strcpy(current->value, value);
+			break;
+		}
+		else
+			current = current->next;
+	}
+	if (current)
+		return (1);
+
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
-
 	node->value = malloc(sizeof(value));
 	if (!node->value)
 	{
 		free(node);
 		return (0);
 	}
+
 	strcpy(node->value, value);
-
-	node->key = malloc(sizeof(key));
-	if (!node->key)
-	{
-		free(node->value);
-		free(node);
-		return (0);
-	}
-	strcpy(node->key, key);
-
-	index = key_index((const unsigned char *)node->key, ht->size);
-	node->next = NULL;
-	if (ht->array[index])
-	{
-		node->next = ht->array[index];
-		ht->array[index] = node;
-	}
-	else
-		ht->array[index] = node;
+	node->key = (char *)key;
+	node->next = ht->array[index];
+	ht->array[index] = node;
 
 	return (1);
 }
