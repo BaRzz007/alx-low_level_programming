728c4cf39bfd440b7c0d0fb035850c9e6208c6f7
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-03-23 04:37:49 +0100

    More hot fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,9 @@
 
 def island_perimeter(grid):
     """Function calculated the perimeter of an island in grid"""
+    if len(grid) < 0:
+        return 0
+
     height = len(grid)
     width = len(grid[0])
     perimeter = 0
@@ -15,12 +18,14 @@ def island_perimeter(grid):
                 if (row == 0) or (row > 0 and grid[row - 1][col] == 0):
                     perimeter += 1
                 # check South
-                if (row == height) or (row < height and grid[row + 1][col] == 0):
+                if (row == height) or\
+                        (row < height and grid[row + 1][col] == 0):
                     perimeter += 1
                 # check West
                 if (col == 0) or (col > 0 and grid[row][col - 1] == 0):
                     perimeter += 1
                 # check East
-                if (col == width) or (col < width and grid[row][col + 1] == 0):
+                if (col == width) or\
+                        (col < width and grid[row][col + 1] == 0):
                     perimeter += 1
     return perimeter
