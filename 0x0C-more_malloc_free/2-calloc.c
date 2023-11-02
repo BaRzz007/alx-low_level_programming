e80dcd17308cf5c4aca5c4d11ef31615a91e2366
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-11-02 12:43:17 +0100

    Done all tasks

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _calloc - Allocates memory for an array using malloc
+ * @nmemb: number of elements in the array
+ * @size: size of each array element
+ * Return: return pointer to the newly allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+
+	memset(p, 0, nmemb * size);
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - Creates an array of integers
+ * @min: size of small array
+ * @max: size of big array
+ * Return: pointer to newly created array
+ */
+int *array_range(int min, int max)
+{
+	int i, size;
+	int *p;
+
+	if (min > max)
+		return (NULL);
+
+	size = (max - min) + 1;
+
+	p = malloc(sizeof(int) * size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; min <= max; i++)
+	{
+		p[i] = min;
+		min++;
+	}
+
+	return (p);
+}
