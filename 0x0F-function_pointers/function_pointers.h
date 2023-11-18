d339ef2a8302dbee6f37d331740c32453eb13e68
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-11-18 15:28:17 +0100

    betty style fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
-#include <stddef.h>
 #ifndef FUNCTIONS
 #define FUNCTIONS
+
+#include <stddef.h>
+
 int _putchar(char);
 void print_name(char *, void (*f)(char *));
 void array_iterator(int *arr, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
 #endif
