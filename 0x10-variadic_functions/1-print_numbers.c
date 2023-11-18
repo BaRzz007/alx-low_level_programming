5f9fb7bbe0f48d635ccd23ef7e300e640e0e4814
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-11-18 15:29:45 +0100

    Done all tasks

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers, followed by a new line.
+ * @seperator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	if (seperator && n > 0)
+	{
+		printf("%d", va_arg(args, int));
+		for (i = 1; i < n; i++)
+		{
+			printf("%s%d", seperator, va_arg(args, int));
+		}
+	}
+	else if (!seperator && n > 0)
+	{
+		for (i = 0; i < n; i++)
+			printf("%d", va_arg(args, int));
+	}
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+Variadic functions - A way to design functions that take in user defined number of variables
